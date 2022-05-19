afafa0036a043f60796856d9d04ebd3b26625112
slyni <slyni@naver.com>
2022-05-20 03:52:02 +0900

    Summary (required)

--- gframe/utils_gui.cpp
+++ gframe/utils_gui.cpp
@@ -141,7 +141,7 @@ irr::IrrlichtDevice* GUIUtils::CreateDevice(GameConfig* configs) {
 #endif
 	driver->setTextureCreationFlag(irr::video::ETCF_CREATE_MIP_MAPS, false);
 	driver->setTextureCreationFlag(irr::video::ETCF_OPTIMIZED_FOR_QUALITY, true);
-	device->setWindowCaption(L"Project Ignis: EDOPro");
+	device->setWindowCaption(L"Project Ignis: TsukasaPro");
 #if !(IRRLICHT_VERSION_MAJOR==1 && IRRLICHT_VERSION_MINOR==9)
 	device->setResizable(true);
 #endif
